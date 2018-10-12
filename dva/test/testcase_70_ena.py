34243796e227265f59240369143b15b776329dc9
Pavlina <pbartiko@redhat.com>
2018-10-12 18:26:01 +0200

    testcase_70_ena updated - m5 instances have ENA driver

--- dva/test/testcase_70_ena.py
+++ dva/test/testcase_70_ena.py
@@ -7,7 +7,7 @@ class testcase_70_ena(Testcase):
     """
     stages = ['stage1']
     tags = ['default']
-    applicable = {"platform": "(?i)RHEL|BETA", "version": "OS (>=7.4)",  'cloudhwname': 'm4.16xlarge|x1.16xlarge'}
+    applicable = {"platform": "(?i)RHEL|BETA", "version": "OS (>=7.4)",  'cloudhwname': 'm4.16xlarge|x1.16xlarge|m5.xlarge'}
     not_applicable = {'product': '(?i)ATOMIC'}
 
     # pylint: disable=unused-argument
