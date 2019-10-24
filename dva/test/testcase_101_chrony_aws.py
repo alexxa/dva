d40757f11549dc419db261a5cc5ac7a7ac3b1255
Xiao Liang <xiliang@redhat.com>
2019-10-24 17:15:52 +0800

    added testcase_101_chrony_aws
    Signed-off-by: Xiao Liang <xiliang@redhat.com>

---
+++ dva/test/testcase_101_chrony_aws.py
@@ -0,0 +1,19 @@
+""" This module contains testcase_362_nvme_iotimeout test """
+from testcase import Testcase
+from os import path
+
+
+class testcase_101_chrony_aws(Testcase):
+    """
+    Bug 1679763, [RFE] AWS AMI - Add Amazon Timesync Service
+    """
+    stages = ['stage1']
+    tags = ['default']
+    applicable = {'cloudhwname': 't3.large|m5.large|m5.xlarge'}
+
+    # pylint: disable=W0613
+    def test(self, connection, params):
+        """ Perform test """
+        self.get_result(connection, 'cat /etc/chrony.conf', timeout=20)
+        self.get_return_value(connection, 'grep \'server 169.254.169.123\' /etc/chrony.conf')
+        return self.log
